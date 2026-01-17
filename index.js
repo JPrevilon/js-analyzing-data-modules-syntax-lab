a467f0cdd5d214ec899bf0680ff9644b5fb5b3da
JPrevilon <joshuaprevilon13@gmail.com>
2026-01-17 14:32:31 -0500

    Finish combineUsers lab

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+require("datejs");
+function combineUsers(...args) {
+  // This is the object we will return
+  const combinedObject = {
+    users: []
+  };
+
+  // Go through each array of usernames
+  args.forEach((userArray) => {
+    // Merge the usernames into the users list
+    combinedObject.users = [...combinedObject.users, ...userArray];
+  });
+
+  // Add today's date in the required format
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 
 
